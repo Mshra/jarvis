49d84c0e30ca6e1dbaace73b6391a850c9576ecc
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-06-29 12:39:03 +0530

    extracted out placeholder and implementing box for ai response

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,37 +1,32 @@
 import { useState, useEffect } from "react";
 import searchIcon from './assets/search.svg'
 import closeIcon from './assets/close.svg'
-import { run } from "./gemini";
+import { generatePlaceholder } from "./placeholder";
+import { AIbox } from "./Gemini";
 
 export default function SearchBar({ theme }: { theme: "light" | "dark" }) {
-  const placeholder: Array<string> = [
-    "Ask me anything...",
-    "Type your question here...",
-    "How can I help you?",
-    "Search with power of AI...",
-    "What do you need to know?"
-  ];
+  const [placeholder, setPlaceholder] = useState<string>(generatePlaceholder(null))
+  // const [AIresponse, setAIresponse] = useState(null)
 
   const lightStyle: string = "flex items-center justify-center h-10 w-full p-2 border-4 border-black rounded-md group"
   const errorStyle: string = "flex items-center justify-center h-10 w-full p-2 border-4 border-red-500 rounded-md group"
   const darkStyle: string = "flex items-center justify-center h-10 w-full p-2 border-4 border-white rounded-md group"
+  const [formStyle, setFormStyle] = useState<string>(lightStyle)
 
-  const [placeholderString, setPlaceholderString] = useState<string>(placeholder[0])
-  const [prompt, setPrompt] = useState<string>("")
+  const [prompt, setPrompt] = useState<string | null>(null)
   const [promptHistory, setPromptHistory] = useState<Array<string>>(new Array())
-  const [formStyle, setFormStyle] = useState<string>(theme === "light" ? lightStyle : darkStyle)
 
+  // changes placeholder value after every 3 seconds
   useEffect(() => {
-    function changePlaceholderString() {
-      const _index: number = placeholder.indexOf(placeholderString)
-      const _new_placeholder: Array<string> = placeholder.slice(0, _index).concat(placeholder.slice(_index + 1, 5))
-      setPlaceholderString(_new_placeholder[Math.floor(Math.random() * 4)])
-    }
-    setInterval(changePlaceholderString, 3000)
+    setInterval(() => setPlaceholder(generatePlaceholder(placeholder)), 3000)
   }, [])
 
-  function handleSubmit(event: any) {
-    event?.preventDefault()
+  function handleSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    if (prompt == null) {
+      return
+    }
 
     if (prompt.length > 10) {
       if (promptHistory.find(pastPrompts => pastPrompts == prompt)) {
@@ -41,7 +36,7 @@ export default function SearchBar({ theme }: { theme: "light" | "dark" }) {
       const _arr = promptHistory.slice()
       _arr.push(prompt)
       setPromptHistory(_arr)
-      // run(prompt) -- runs the generative ai
+      // TODO: run the AI
     } else {
       console.error('prompt length not sufficient')
       setFormStyle(errorStyle)
@@ -58,11 +53,19 @@ export default function SearchBar({ theme }: { theme: "light" | "dark" }) {
     }
 
     const mapPromptHistory = promptHistory.map(promptString =>
-      <li key={Math.random() * 10} onClick={() => setPrompt(promptString)} className="bg-[#fecdd3] overflow-y-auto my-1 px-1 flex items-center justify-center last:rounded-b-md hover:border hover:border-black">
+      <li key={Math.random() * 10} onClick={() => setPrompt(promptString)}
+        className="bg-[#fecdd3] overflow-y-auto my-1 px-1 flex items-center justify-center last:rounded-b-md hover:border hover:border-black">
+
         <div className="w-11/12 break-all">
           {promptString}
         </div>
-        <input type="image" src={closeIcon} alt="close" onClick={handlePromptDelete} className="w-1/12 h-10" />
+
+        <input
+          type="image"
+          src={closeIcon}
+          alt="close"
+          onClick={handlePromptDelete}
+          className="w-1/12 h-10" />
       </li>
     )
 
@@ -83,20 +86,34 @@ export default function SearchBar({ theme }: { theme: "light" | "dark" }) {
 
   return (
     <div className="w-9/12 md:w-3/6 lg:w-2/6 flex-col">
+      {/* <AIbox prompt={prompt} /> */}
+
       <form onSubmit={handleSubmit} className={formStyle}>
-        <input type="text" placeholder={placeholderString} className="outline-none w-11/12" value={prompt} onChange={handleChange} />
-        <input type="image" src={searchIcon} alt="search" className="h-8" />
+        <input
+          type="text"
+          placeholder={placeholder}
+          className="outline-none w-11/12"
+          value={prompt as string}
+          onChange={handleChange} />
+
+        <input
+          type="image"
+          src={searchIcon}
+          alt="search"
+          className="h-8" />
       </form>
+
       {prompt && handlePromptHistory()}
+
     </div>
   )
 }
 
 /*
- * TODO: [ ] refactor out the styles into light mode, dark mode and error mode into a different file.
- *       [ ] gemini is returning non-continuous response as markdown, to display the response convert it into either html or normal text atleast.
+ * TODO: [ ] gemini is returning non-continuous response as markdown, to display the response convert it into either html or normal text atleast.
  *       [ ] style prompt history lists
  *       [ ] animate the placeholders with a typing effect
+ *       [ ] refactor out the styles into light mode, dark mode and error mode into a different file.
  *
  * FIX: [x] promptHistory allow duplicate values, prevent that.
  */
