b681f190e3b7b2fcf6beee16d80093f7de08f677
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-06-30 20:48:48 +0530

    made prompt appear below the search bar

--- src/components/PromptHistory.tsx
+++ src/components/PromptHistory.tsx
@@ -2,7 +2,6 @@
 //       [ ]
 
 import { SetStateAction } from 'react'
-import closeIcon from './assets/close.svg'
 
 function PromptHistory({ setPromptString, promptHistory, setPromptHistory }: { setPromptString: React.Dispatch<SetStateAction<string>>, promptHistory: Array<string>, setPromptHistory: React.Dispatch<SetStateAction<Array<string>>> }) {
 
@@ -10,24 +9,23 @@ function PromptHistory({ setPromptString, promptHistory, setPromptHistory }: { s
     const _history: Array<string> = promptHistory.slice()
     _history.splice(index, 1)
     setPromptHistory(_history)
+    setPromptString("")
   }
 
+  // w-1/12 h-10 m-2 
   const mapPromptHistory = promptHistory.map((promptString, index) => {
     return (
-      <li key={index}
-        onClick={() => setPromptString(promptString)}
-        className="bg-[#fecdd3] overflow-y-auto my-1 px-1 flex items-center justify-center last:rounded-b-md hover:border hover:border-black">
+      <li key={index} className="bg-[#fecdd3] overflow-y-auto my-1 px-4 flex items-center justify-center last:rounded-b-md hover:border hover:border-black">
 
-        <div className="w-11/12 break-all">
+        <div className="w-11/12 break-all" onClick={() => setPromptString(promptString)}>
           {promptString}
         </div>
 
-        <input
-          type="image"
-          src={closeIcon}
-          alt="close"
+        <button
           onClick={() => handleDeletePromptFromHistory(index)}
-          className="w-1/12 h-10" />
+          className="px-2 py-1 m-2 border border-black rounded-md bg-red-400 text-red-800 font-extrabold hover: hover:bg-red-600 hover:text-white">
+          X
+        </button>
       </li>
     );
   });
@@ -35,7 +33,7 @@ function PromptHistory({ setPromptString, promptHistory, setPromptHistory }: { s
   // returns for display only when atleast 1 prompt exists in history
   if (promptHistory.length > 0) {
     return (
-      <ul className="w-full h-auto border-x-2 border-b-2 border-black -mt-2 pt-2 px-1 rounded-b-lg">
+      <ul className="w-full h-auto border-x-2 border-b-2 border-black -mt-2 pt-2 px-1 rounded-b-lg absolute">
         {mapPromptHistory}
       </ul>
     )
