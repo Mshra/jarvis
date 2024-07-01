7f447ed4ccfa2b446de3e01bb6f822d8c30a457e
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-07-01 10:06:12 +0530

    added docs

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,18 +1,23 @@
-/*
- * TODO: [ ] gemini is returning non-continuous response as markdown, to display the response convert it into either html or normal text atleast.
- *       [ ] style prompt history lists
- *       [ ] refactor out the styles into light mode, dark mode and error mode into a different file.
- *
- * FIX: [x] promptHistory allow duplicate values, prevent that.
- */
-import { ResponseBox } from "./AI";
-
 import React, { SetStateAction, useState } from "react";
 import searchIcon from './assets/search.svg'
 import { PromptHistory } from "./PromptHistory";
 import { lightStyle } from "./styles";
 
+/**
+ * handles addition of prompt string to promptHistory, is one of the procedures carried out after 'onSubmit' of the string in search bar.
+ *
+ * @param {string} promptString - the entered prompt for AI response just after 'onSubmit'.
+ * @param {Array<string>} promptHistory - the past entered prompt strings which is stored in an array.
+ * @param {React.Dispatch<SetStateAction<string>>} setPromptString - changes the prompt string in search bar.
+ * @param {React.Dispatch<SetStateAction<Array<string>>>} setPromptHistory - changes the prompt elements in 'promptHistory' array.
+ */
 function addPromptToHistory(promptString: string, promptHistory: Array<string>, setPromptString: React.Dispatch<SetStateAction<string>>, setPromptHistory: React.Dispatch<SetStateAction<Array<string>>>): void {
+  /**
+   * If the entered prompt is already present in the past then does nothing.
+   * otherwise, creates a duplicate array of the promptHistory, reverses it to present recently entered prompts at last,
+   * reverses it to get them in ascending order, pushes the 'promptString' at last
+   * again reverses it to have the array in descending order.
+   */
   if (promptHistory.find(prompts => prompts === promptString)) { }
   else {
     const _arr = promptHistory.slice()
@@ -21,20 +26,29 @@ function addPromptToHistory(promptString: string, promptHistory: Array<string>, 
     _arr.reverse()
     setPromptHistory(_arr)
   }
+
   setPromptString("")
+
+  // clips out the 'promptHistory' to store exactly 5 number of strings by dropping the past entered prompts one by one.
 }
 
-// main component
+/**
+ * Main component.
+ */
 export default function SearchBar() {
   // this state is used by PromptHistory.tsx aswell
   const [promptHistory, setPromptHistory] = useState<Array<string>>(new Array())
   const [promptString, setPromptString] = useState<string>("")
 
+  /**
+   * @param {React.SyntheticEvent<HTMLFormElement>} event - event described by the submission of form.
+   * -> adds the entered 'promptString' to the 'promptHistory' for display.
+   * -> passes the 'promptString' for the generation of AI response.
+   */
   function handleSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
     event.preventDefault()
     addPromptToHistory(promptString, promptHistory, setPromptString, setPromptHistory)
     // TODO: generating ai respose to prompt string
-    run("what is git")
   }
 
   return (
@@ -55,7 +69,14 @@ export default function SearchBar() {
       </form>
 
       {promptString ? <PromptHistory setPromptString={setPromptString} promptHistory={promptHistory} setPromptHistory={setPromptHistory} /> : null}
-      {/* <ResponseBox prompt="what is git" /> */}
     </div >
   )
 }
+
+/*
+ * TODO: [ ] gemini is returning non-continuous response as markdown, to display the response convert it into either html or normal text atleast.
+ *       [ ] style prompt history lists
+ *       [ ] refactor out the styles into light mode, dark mode and error mode into a different file.
+ *
+ * FIX: [x] promptHistory allow duplicate values, prevent that.
+ */
