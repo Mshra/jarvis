d6c8c8ad41d32847c5727056d39daa0a8c055c5e
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-07-01 16:08:31 +0530

    changing layer positions

--- src/components/styles.ts
+++ src/components/styles.ts
@@ -1,3 +1,3 @@
-const lightStyle: string = "flex items-center justify-center h-10 w-full p-2 border-4 border-black rounded-md group"
+const lightStyle: string = "flex items-center justify-center h-10 w-full p-2 border-4 border-black rounded-md group z-20"
 
 export { lightStyle }
